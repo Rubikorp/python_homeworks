6f31c26a71d7d0c31f3dc38dda51708ed37ba063
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-12-07 19:30:11 +0500

    Задача 5.2

---
+++ HomeWork_5/Task_5.2.py
@@ -0,0 +1,9 @@
+my_file = 'Task_5.2.txt'
+
+with open(my_file, 'r', encoding='utf-8') as data:
+    stroces = [line for line in data]
+    print(f'в документе {my_file}: {len(stroces)} строки')
+    num_stroke = 0
+    for stroce in stroces:
+        num_stroke += 1
+        print(f'В строке {num_stroke}: {len(stroce.split())} слова')
