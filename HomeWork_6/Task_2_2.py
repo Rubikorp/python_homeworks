477d697f3780250d98dd76a17df9088b8f802230
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-12-10 20:08:07 +0500

    Задача_6.1 замер времени до изменений 2_5 - 0.0002266999799758196 2_2 - 0.00036189984530210495

---
+++ HomeWork_6/Task_2_2.py
@@ -0,0 +1,49 @@
+__autor__ = 'Ruslan Bikmetov'
+
+"""
+Задача 2
+Для списка реализовать обмен значений соседних элементов, 
+т.е. значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д. 
+При нечетном количестве элементов последний сохранить на своем месте. 
+Для заполнения списка элементов необходимо использовать функцию input().
+"""
+
+# index = 0
+# element = input(f"Введите элемент {index}:")
+# array = []
+# while element != "end":  # Закрыть ввод
+#     array.append(element)
+#     index += 1
+#     element = input(f"Введите элемент {index}:")
+#
+# print(array)
+#
+#
+# def sort_array(arr):
+#     length = len(arr)
+#     ind = 0
+#     if length % 2 != 0:
+#         length -= 1
+#     while length > ind:
+#         x = arr[ind]
+#         arr[ind] = arr[ind + 1]
+#         arr[ind + 1] = x
+#         ind += 2
+#
+#
+# sort_array(array)
+# print(array)
+
+arr = [1, 2, 3, 4, 5, 6]
+
+
+def sort_array(arr):
+    length = len(arr)
+    ind = 0
+    if length % 2 != 0:
+        length -= 1
+    while length > ind:
+        x = arr[ind]
+        arr[ind] = arr[ind + 1]
+        arr[ind + 1] = x
+        ind += 2
