7e8587ed995333024ffd952e72c060bb38d9b4ba
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-12-16 19:54:17 +0500

    create branch HomeWork_7 and Tasks

--- HomeWork_6/Task_4_2.py
+++ HomeWork_6/Task_4_2.py
@@ -1,12 +1,3 @@
-'''
-Задача 2
-Представлен список чисел. Необходимо вывести элементы исходного списка,
-значения которых больше предыдущего элемента.
-Подсказка: элементы, удовлетворяющие условию,
-оформить в виде списка. Для формирования списка использовать генератор.
-Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
-Результат: [12, 44, 4, 10, 78, 123].
-'''
 from random import randint
 
 from memory_profiler import profile
@@ -14,7 +5,7 @@ from memory_profiler import profile
 
 @profile
 def create_sort_arr():
-    """Выделяет доп память, не освобождается"""
+    """Выделяет память, доп память не выделяется"""
     my_list = [randint(1, 100) for x in range(1, 10)]
     min_el = my_list[0]
     my_list2 = []
@@ -25,27 +16,30 @@ def create_sort_arr():
 
     return my_list2
 
+
+create_sort_arr()
+
 '''
 Line #    Mem usage    Increment  Occurrences   Line Contents
 =============================================================
-    15     22.0 MiB     22.0 MiB           1   @profile
-    16                                         def create_sort_arr():
-    17                                             """Выделяет доп память, не освобождается"""
-    18     22.0 MiB      0.0 MiB          12       my_list = [randint(1, 100) for x in range(1, 10)]
-    19     22.0 MiB      0.0 MiB           1       min_el = my_list[0]
-    20     22.0 MiB      0.0 MiB           1       my_list2 = []
-    21     22.0 MiB      0.0 MiB          10       for el in my_list:
-    22     22.0 MiB      0.0 MiB           9           if el > min_el:
-    23     22.0 MiB      0.0 MiB           3               my_list2.append(el)
-    24     22.0 MiB      0.0 MiB           9           min_el = el
-    25                                         
-    26     22.0 MiB      0.0 MiB           1       return my_list2
+     6     22.3 MiB     22.3 MiB           1   @profile
+     7                                         def create_sort_arr():
+     8                                             """Выделяет память, доп память не выделяется"""
+     9     22.3 MiB      0.0 MiB          12       my_list = [randint(1, 100) for x in range(1, 10)]
+    10     22.3 MiB      0.0 MiB           1       min_el = my_list[0]
+    11     22.3 MiB      0.0 MiB           1       my_list2 = []
+    12     22.3 MiB      0.0 MiB          10       for el in my_list:
+    13     22.3 MiB      0.0 MiB           9           if el > min_el:
+    14     22.3 MiB      0.0 MiB           4               my_list2.append(el)
+    15     22.3 MiB      0.0 MiB           9           min_el = el
+    16                                         
+    17     22.3 MiB      0.0 MiB           1       return my_list2
 '''
 
 
 @profile
 def create_sort_arr():
-    """Выделяет доп память, освобождается"""
+    """Выделяет память, доп память не выделяется"""
     my_list = [randint(1, 100) for x in range(1, 10)]
     min_el = my_list[0]
     my_list2 = []
@@ -54,29 +48,30 @@ def create_sort_arr():
             my_list2.append(el)
         min_el = el
 
-    del my_list
     min_el = None
+    del my_list
 
     return my_list2
+
 
 '''
 Line #    Mem usage    Increment  Occurrences   Line Contents
 =============================================================
-    46     22.1 MiB     22.1 MiB           1   @profile
-    47                                         def create_sort_arr():
-    48                                             """Выделяет доп память, освобождается"""
-    49     22.2 MiB      0.0 MiB          12       my_list = [randint(1, 100) for x in range(1, 10)]
-    50     22.2 MiB      0.0 MiB           1       min_el = my_list[0]
-    51     22.2 MiB      0.0 MiB           1       my_list2 = []
-    52     22.2 MiB      0.0 MiB          10       for el in my_list:
-    53     22.2 MiB      0.0 MiB           9           if el > min_el:
-    54     22.2 MiB      0.0 MiB           3               my_list2.append(el)
-    55     22.2 MiB      0.0 MiB           9           min_el = el
-    56                                         
-    57     22.2 MiB      0.0 MiB           1       del my_list
-    58     22.2 MiB      0.0 MiB           1       min_el = None
-    59                                         
-    60     22.2 MiB      0.0 MiB           1       return my_list2
+    40     22.3 MiB     22.3 MiB           1   @profile
+    41                                         def create_sort_arr():
+    42                                             """Выделяет память, доп память не выделяется"""
+    43     22.3 MiB      0.0 MiB          12       my_list = [randint(1, 100) for x in range(1, 10)]
+    44     22.3 MiB      0.0 MiB           1       min_el = my_list[0]
+    45     22.3 MiB      0.0 MiB           1       my_list2 = []
+    46     22.3 MiB      0.0 MiB          10       for el in my_list:
+    47     22.3 MiB      0.0 MiB           9           if el > min_el:
+    48     22.3 MiB      0.0 MiB           4               my_list2.append(el)
+    49     22.3 MiB      0.0 MiB           9           min_el = el
+    50                                         
+    51     22.3 MiB      0.0 MiB           1       min_el = None
+    52     22.3 MiB      0.0 MiB           1       del my_list
+    53                                         
+    54     22.3 MiB      0.0 MiB           1       return my_list2
 '''
 
 create_sort_arr()
